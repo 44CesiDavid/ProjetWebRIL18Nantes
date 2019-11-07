19551b7e0d6f3c87ea1cedacbd0ad7f1d3bbd054
44CesiDavid <49159703+44CesiDavid@users.noreply.github.com>
2019-11-07 11:37:28 +0100

    Ajout Connexion.php

---
+++ contenu/connection.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- META INFORMATIONS-->
+    <title>Voltik</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="Voltik">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--BOOSTRAP-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap4.3.1.css" />
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body >
+
+			<!--HEADER-->
+
+    <h1>Connexion</h1>
+        <br/>
+        <form class="formulaireConnexion">
+            <div>
+                    <label><b>Nom d'utilisateur</b></label>
+                    <input type="text" name="nomUser">
+            </div>
+            <br/>
+            <div>
+                    <label><b>Mot de passe</b></label>
+                    <input type="password" name="pwdUser">
+            </div>
+            <br/>
+            <input type="submit" value="Se connecter">
+        </form>
+
+
+
+
+		<!--FOOTER-->
+
+
+
+
+		<!--SCRIPTS-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+		<script src="js/jquery-3.4.1.min.js"></script>
+		<script src="js/bootstrap-4.3.1.min.js"></script>
+		<script src="js/carousel.js"></script>
+
+		<!-- WEBSITE JAVASCRIPT -->
+		<script src="js/headers/mainNav.js"></script>
+		<script src="js/footers/footer.js"></script>
+
+</body>
+
+</html>
